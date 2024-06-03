0cd0ea151bf8f6d9623ad1eb4d3c5a3e013a67c6
Theng Hongsour <thenghongsour@gmail.com>
2024-06-03 22:18:44 +0700

    Build toggle menu bar (NavBar)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,19 @@
-import { Center, HStack, Image, List, ListItem, Show } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Show,
+} from "@chakra-ui/react";
 import logo from "../assets/images/logo.svg";
 import iconMenu from "../assets/images/icon-menu.svg";
+import iconClose from "../assets/images/icon-menu-close.svg";
 import menuItem from "../data/menu";
+import { useState } from "react";
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <Center>
       <HStack
@@ -11,15 +22,67 @@ const NavBar = () => {
           base: "96%",
           sm: "80%",
         }}
+        mt={1}
       >
         <List>
           <ListItem>
-            <Image cursor="pointer" src={logo} alt="logo" boxSize="40px" />
+            <Image cursor="pointer" src={logo} alt="logo" boxSize="35px" />
           </ListItem>
         </List>
         <List display="flex" cursor="pointer">
           <Show below="sm">
-            <Image src={iconMenu} alt="icon_menu" boxSize="20px" />
+            {toggle ? (
+              <>
+                <List
+                  position="absolute"
+                  top="0"
+                  right="0"
+                  bg="hsl(36, 100%, 99%)"
+                  zIndex="3"
+                  w="250px"
+                  h="100vh"
+                >
+                  <Image
+                    cursor="pointer"
+                    p={5}
+                    float="right"
+                    src={iconClose}
+                    alt="icon_menu"
+                    boxSize="65px"
+                    onClick={() => setToggle(!toggle)}
+                  />
+                  <Box mt="120px" ml={2}>
+                    {menuItem.map((menu) => (
+                      <ListItem
+                        fontFamily="inter-bold"
+                        fontSize="md"
+                        p={2}
+                        key={menu.id}
+                      >
+                        {menu.name}
+                      </ListItem>
+                    ))}
+                  </Box>
+                </List>
+                <Box
+                  w="100%"
+                  h="100vh"
+                  zIndex="2"
+                  bg="rgba(0, 0, 0, 0.208)"
+                  position="absolute"
+                  top='0'
+                  left='0'
+                  onClick={() => setToggle(!toggle)}
+                />
+              </>
+            ) : (
+              <Image
+                src={iconMenu}
+                alt="icon_menu"
+                boxSize="20px"
+                onClick={() => setToggle(!toggle)}
+              />
+            )}
           </Show>
           <Show above="sm">
             {menuItem.map((menu) => (
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
+import Test from "./components/Test";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: "error",
+  },
+  {
+    path: "test",
+    element: <Test />,
   },
 ]);
 export default router;
