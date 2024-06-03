c9918dad4d07b715269afb5f3ca90713c8bb98f2
Theng Hongsour <thenghongsour@gmail.com>
2024-06-03 15:55:34 +0700

    Build NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,46 @@
-
+import { Center, HStack, Image, List, ListItem, Show } from "@chakra-ui/react";
+import logo from "../assets/images/logo.svg";
+import iconMenu from "../assets/images/icon-menu.svg";
+import menuItem from "../data/menu";
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Center>
+      <HStack
+        justifyContent="space-between"
+        w={{
+          base: "96%",
+          sm: "80%",
+        }}
+      >
+        <List>
+          <ListItem>
+            <Image cursor="pointer" src={logo} alt="logo" boxSize="40px" />
+          </ListItem>
+        </List>
+        <List display="flex" cursor="pointer">
+          <Show below="sm">
+            <Image src={iconMenu} alt="icon_menu" boxSize="20px" />
+          </Show>
+          <Show above="sm">
+            {menuItem.map((menu) => (
+              <ListItem
+                _hover={{
+                  transition: " 0.3s ease",
+                  color: "hsl(35, 77%, 62%)",
+                }}
+                fontFamily="inter-regular"
+                fontSize="sm"
+                p={3}
+                key={menu.id}
+              >
+                {menu.name}
+              </ListItem>
+            ))}
+          </Show>
+        </List>
+      </HStack>
+    </Center>
+  );
 };
 
 export default NavBar;
---
+++ src/data/menu.ts
@@ -0,0 +1,28 @@
+const menuItem = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "New",
+    path: "new",
+  },
+  {
+    id: 3,
+    name: "Popular",
+    path: "popular",
+  },
+  {
+    id: 4,
+    name: "Trending",
+    path: "trending",
+  },
+  {
+    id: 5,
+    name: "Categories",
+    path: "categories",
+  },
+];
+export default menuItem;
